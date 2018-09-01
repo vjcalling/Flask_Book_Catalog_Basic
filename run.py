62af3492c685838e8d750b726488a7a87f578a86
vjcalling <vibhor.jain@outlook.com>
2018-09-01 19:06:56 +0530

    adding html code

--- run.py
+++ run.py
@@ -5,14 +5,132 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config.update(
-    SECRET_KEY='password',
-    SQLALCHEMY_DATABASE_URI='postgresql://postgres:password@localhost/catalog_db',
+    SECRET_KEY='topsecret',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:topsecret@localhost/catalog_db',
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
 db = SQLAlchemy(app)
 
 
+@app.before_request
+def some_function():
+    g.string = '<br> This code ran before any request'
+
+
+# BASIC ROUTE
+@app.route('/index')
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask! <br>' + g.string
+
+
+# QUERY STRINGS
+@app.route('/new/')
+def query_strings(greeting='hello'):
+    query_val = request.args.get('greeting', greeting)
+    return '<h1> the greeting is : {0} </h1>'.format(query_val) + g.string
+
+
+# REMOVE QUERY STRINGS
+@app.route('/user')
+@app.route('/user/<name>')
+def no_query_strings(name='mina'):
+    return '<h1> hello there ! {} </h1>'.format(name)
+
+
+# STRINGS
+@app.route('/text/<string:name>')
+def working_with_strings(name):
+    return '<h1> here is a string: ' + name + '</h1>'
+
+
+# NUMBERS
+@app.route('/numbers/<int:num>')
+def working_with_numbers(num):
+    return '<h1> the number you picked is: ' + str(num) + '</h1>'
+
+
+# MORE NUMBERS
+@app.route('/add/<int:num1>/<int:num2>')
+def adding_integers(num1, num2):
+    return '<h1>the sum is : {}'.format(num1 + num2) + '</h1>'
+
+
+# FLOATS
+@app.route('/product/<float:num1>/<float:num2>')
+def product_two_numbers(num1, num2):
+    return '<h1> the product is: {}'.format(num1 * num2) + '</h1>'
+
+
+# USING TEMPLATES
+@app.route('/temp')
+def using_templates():
+    return render_template('hello.html')
+
+
+# JINJA TEMPLATES
+@app.route('/watch')
+def movies_2017():
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
+
+    return render_template('movies.html',
+                           movies=movie_list,
+                           name='Harry')
+
+
+@app.route('/tables')
+def movies_plus():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('table_data.html',
+                           movies=movies_dict,
+                           name='Sally')
+
+
+@app.route('/filters')
+def filter_data():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name=None,
+                           film='a christmas carol')
+
+
+@app.route('/macros')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html',
+                           movies=movies_dict)
+
+
+@app.route('/session')
+def session_data():
+    if 'name' not in session:
+        session['name'] = 'harry'
+    return render_template('session.html', name=session['name'], session=session)
 
 
 class Publication(db.Model):
@@ -54,36 +172,6 @@ class Book(db.Model):
 
     def __repr__(self):
         return '{} by {}'.format(self.title, self.author)
-
-
-
-
-
-@app.route('/')
-def hello_flask():
-    return 'Hello Flask'
-
-@app.route('/greet')
-def greet_flask():
-    return 'Hello Vibhor'
-
-
-@app.route('/new')
-def query_strings(greeting='hello'):
-    query_val = request.args.get('greeting', greeting)
-    return '<h1>The greeting is: {0}</h1>'.format(query_val)
-
-
-@app.route('/user')
-@app.route('/user/<name>')
-def no_query_strings(name='vj'):
-    return '<h1>Hello there {0}</h1>'.format(name)
-
-
-@app.route('/temp')
-def using_templates():
-    return render_template('hello.html', name='Vibhor')
-
 
 
 if __name__ == '__main__':
